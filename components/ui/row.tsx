38c3eb90af4334a55d560a26446cc47cb2a9a858
Marce <marcekekey@gmail.com>
2025-12-01 01:37:13 +0100

    init publication

--- components/ui/row.tsx
+++ components/ui/row.tsx
@@ -1,4 +1,4 @@
-export function Row({ label, value }) {
+export function Row({ label, value }:{ label:string | React.ReactNode , value:string | React.ReactNode }) {
   return (
     <div className="flex items-center justify-between"><div className="text-slate-600">{label}</div><div>{value}</div></div>
   );
